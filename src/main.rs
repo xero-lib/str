9bfd236f057eb18db8de9c882950491188173216
thoth@damocles <jonathankurtz@protonmail.com>
2025-03-26 01:06:01 -0500

    moved cmdline arg parsing to top of main for global flag parsing

--- src/main.rs
+++ src/main.rs
@@ -370,8 +370,9 @@ struct Args {
 }
 
 fn main() {
+    let operation: Operation = Args::parse().operation.unwrap_or_default(); // call at top to enable flags without stdin
     for i in std::io::stdin().lines().flatten() {
-        match Args::parse().operation.unwrap_or_default().execute(&i) {
+        match operation.execute(&i) {
             Output::Multiple(x) => println!("{}", x.join("\n")),
             Output::Single(x) => println!("{x}"),
         }
