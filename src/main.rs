1184655790f0b5371bb8c5a2ba302b50e773ced3
thoth@damocles <jonathankurtz@protonmail.com>
2025-03-25 21:33:16 -0500

    modularized functions for ease of readability and future separation

--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,8 @@ impl Default for Operation {
 impl Operation {
     pub fn execute(&self, input: &String) -> Output {
         use Operation::*;
+        use op_functions::*;
+
         match self {
             /* Pattern-Based */
             SplitAtWhitespace { number } => split_at_whitespace(*number, input),
@@ -147,213 +149,216 @@ impl Operation {
     }
 }
 
-/* Pattern-Based */
-fn split_at_whitespace(number: Option<i64>, input: &String) -> Output {
-    let trimmed = input.split_whitespace().map(str::to_owned);
+mod op_functions {
+    use crate::Output;
+    /* Pattern-Based */
+    pub fn split_at_whitespace(number: Option<i64>, input: &String) -> Output {
+        let trimmed = input.split_whitespace().map(str::to_owned);
 
-    use Output::*;
-    match number {
-        None => Multiple(input.split_whitespace().map(str::to_owned).collect()),
-        Some(x) if x.is_negative() => Multiple(trimmed.rev().take(x.abs() as usize).collect()), // not exactly intended behavior, collect remaining and return as one entry
-        Some(x) if x.is_positive() => Multiple(trimmed.take(x as usize).collect()),
-        _ => Single(input.to_owned()),
-    }
-}
-
-fn split_at_pat(number: Option<i64>, pattern: &String, input: &String) -> Output {
-    use Output::*;
-    match number {
-        None => Multiple(input.split(pattern).map(str::to_owned).collect()),
-        Some(x) if x.is_negative() => Multiple(
-            input
-                .rsplitn(x.abs() as usize, pattern)
-                .map(str::to_owned)
-                .collect(),
-        ),
-        Some(x) if x.is_positive() => Multiple(
-            input
-                .splitn(x as usize, pattern)
-                .map(str::to_owned)
-                .collect(),
-        ),
-        _ => Single(input.to_owned()),
-    }
-}
-
-fn split_at_char(number: Option<i64>, char: char, input: &String) -> Output {
-    use Output::*;
-    match number {
-        None => Multiple(input.split(char).map(str::to_owned).collect()),
-        Some(x) if x.is_negative() => Multiple(
-            input
-                .rsplitn(x.abs() as usize, char)
-                .map(str::to_owned)
-                .collect(),
-        ),
-        Some(x) if x.is_positive() => {
-            Multiple(input.splitn(x as usize, char).map(str::to_owned).collect())
+        use Output::*;
+        match number {
+            None => Multiple(input.split_whitespace().map(str::to_owned).collect()),
+            Some(x) if x.is_negative() => Multiple(trimmed.rev().take(x.abs() as usize).collect()), // not exactly intended behavior, collect remaining and return as one entry
+            Some(x) if x.is_positive() => Multiple(trimmed.take(x as usize).collect()),
+            _ => Single(input.to_owned()),
         }
-        _ => Single(input.to_owned()),
-    }
-}
-
-fn cut_from_pat(pattern: &String, input: &String) -> Output {
-    let start = input.find(pattern).unwrap_or(0);
-    Output::Single(input[start..].to_string())
-}
-
-fn cut_from_pat_to_pat(start: &String, end: &String, input: &String) -> Output {
-    let start_idx = input.find(start).unwrap_or(0);
-    let end_idx = input.rfind(end).unwrap_or(input.capacity() - 1);
-
-    Output::Single(input[start_idx..end_idx].to_string())
-}
-
-// separate fn for cut from last pat?
-fn cut_from_pat_to_offset(pattern: &String, offset: i64, input: &String) -> Output {
-    let start_idx = input.find(pattern).unwrap_or(0);
-    if offset as usize + start_idx > input.len() - 1 {
-        eprintln!("Offset exits bounds of input");
-        std::process::exit(1); // this could be better with anyhow
     }
 
-    Output::Single(
-        input[if offset.is_negative() {
-            start_idx + (offset.abs() as usize)..start_idx
+    pub fn split_at_pat(number: Option<i64>, pattern: &String, input: &String) -> Output {
+        use crate::Output::*;
+        match number {
+            None => Multiple(input.split(pattern).map(str::to_owned).collect()),
+            Some(x) if x.is_negative() => Multiple(
+                input
+                    .rsplitn(x.abs() as usize, pattern)
+                    .map(str::to_owned)
+                    .collect(),
+            ),
+            Some(x) if x.is_positive() => Multiple(
+                input
+                    .splitn(x as usize, pattern)
+                    .map(str::to_owned)
+                    .collect(),
+            ),
+            _ => Single(input.to_owned()),
+        }
+    }
+
+    pub fn split_at_char(number: Option<i64>, char: char, input: &String) -> Output {
+        use Output::*;
+        match number {
+            None => Multiple(input.split(char).map(str::to_owned).collect()),
+            Some(x) if x.is_negative() => Multiple(
+                input
+                    .rsplitn(x.abs() as usize, char)
+                    .map(str::to_owned)
+                    .collect(),
+            ),
+            Some(x) if x.is_positive() => {
+                Multiple(input.splitn(x as usize, char).map(str::to_owned).collect())
+            }
+            _ => Single(input.to_owned()),
+        }
+    }
+
+    pub fn cut_from_pat(pattern: &String, input: &String) -> Output {
+        let start = input.find(pattern).unwrap_or(0);
+        Output::Single(input[start..].to_string())
+    }
+
+    pub fn cut_from_pat_to_pat(start: &String, end: &String, input: &String) -> Output {
+        let start_idx = input.find(start).unwrap_or(0);
+        let end_idx = input.rfind(end).unwrap_or(input.capacity() - 1);
+
+        Output::Single(input[start_idx..end_idx].to_string())
+    }
+
+    // separate fn for cut from last pat?
+    pub fn cut_from_pat_to_offset(pattern: &String, offset: i64, input: &String) -> Output {
+        let start_idx = input.find(pattern).unwrap_or(0);
+        if offset as usize + start_idx > input.len() - 1 {
+            eprintln!("Offset exits bounds of input");
+            std::process::exit(1); // this could be better with anyhow
+        }
+
+        Output::Single(
+            input[if offset.is_negative() {
+                start_idx + (offset.abs() as usize)..start_idx
+            } else {
+                start_idx..start_idx + (offset as usize)
+            }]
+            .to_string(),
+        )
+    }
+
+    // separate fn for cut until last pat?
+    pub fn cut_until_pat(pattern: &String, input: &String) -> Output {
+        let found_idx = input.find(pattern).unwrap_or(input.len());
+        Output::Single(input[..found_idx].to_string())
+    }
+
+    pub fn trim_from_pat(pattern: &String, input: &String) -> Output {
+        let found_idx = input.find(pattern).unwrap_or(0);
+        Output::Single(input[found_idx..].to_string())
+    }
+
+    pub fn trim_from_pat_to_pat(start: &String, end: &String, input: &String) -> Output {
+        let start_idx = input.find(start).unwrap_or(0);
+        let end_idx = input.rfind(end).unwrap_or(input.len() - 1);
+
+        Output::Single(input[start_idx..end_idx].to_string())
+    }
+
+    // separate fn for trim until last pat?
+    pub fn trim_until_pat(pattern: &String, input: &String) -> Output {
+        let found_idx = input.find(pattern).unwrap_or(0);
+
+        Output::Single(input[found_idx..].to_string())
+    }
+
+    /* Index-Based */
+    pub fn cut_from_index(index: usize, input: &String) -> Output {
+        Output::Single(input[index..].to_string())
+    }
+
+    pub fn cut_from_index_to_index(start: usize, end: usize, input: &String) -> Output {
+        Output::Single(input[start..end].to_string())
+    }
+
+    pub fn cut_from_index_to_offset(index: usize, offset: i64, input: &String) -> Output {
+        Output::Single(
+            input[if offset.is_negative() {
+                index + (offset as usize - 1)..index
+            } else {
+                index..offset as usize
+            }]
+            .to_string(),
+        )
+    }
+
+    pub fn cut_until_index(index: usize, input: &String) -> Output {
+        let index = if index != 0 { index } else { input.len() };
+        Output::Single(input[..index].to_string())
+    }
+
+    pub fn trim_from_index(index: usize, input: &String) -> Output {
+        let index = if index != 0 { index } else { input.len() };
+        Output::Single(input[..index].to_string())
+    }
+
+    pub fn trim_from_index_to_index(start: usize, end: usize, input: &String) -> Output {
+        Output::Single(if end <= start {
+            input.clone()
         } else {
-            start_idx..start_idx + (offset as usize)
-        }]
-        .to_string(),
-    )
-}
-
-// separate fn for cut until last pat?
-fn cut_until_pat(pattern: &String, input: &String) -> Output {
-    let found_idx = input.find(pattern).unwrap_or(input.len());
-    Output::Single(input[..found_idx].to_string())
-}
-
-fn trim_from_pat(pattern: &String, input: &String) -> Output {
-    let found_idx = input.find(pattern).unwrap_or(0);
-    Output::Single(input[found_idx..].to_string())
-}
-
-fn trim_from_pat_to_pat(start: &String, end: &String, input: &String) -> Output {
-    let start_idx = input.find(start).unwrap_or(0);
-    let end_idx = input.rfind(end).unwrap_or(input.len() - 1);
-
-    Output::Single(input[start_idx..end_idx].to_string())
-}
-
-// separate fn for trim until last pat?
-fn trim_until_pat(pattern: &String, input: &String) -> Output {
-    let found_idx = input.find(pattern).unwrap_or(0);
-
-    Output::Single(input[found_idx..].to_string())
-}
-
-/* Index-Based */
-fn cut_from_index(index: usize, input: &String) -> Output {
-    Output::Single(input[index..].to_string())
-}
-
-fn cut_from_index_to_index(start: usize, end: usize, input: &String) -> Output {
-    Output::Single(input[start..end].to_string())
-}
-
-fn cut_from_index_to_offset(index: usize, offset: i64, input: &String) -> Output {
-    Output::Single(
-        input[if offset.is_negative() {
-            index + (offset as usize - 1)..index
-        } else {
-            index..offset as usize
-        }]
-        .to_string(),
-    )
-}
-
-fn cut_until_index(index: usize, input: &String) -> Output {
-    let index = if index != 0 { index } else { input.len() };
-    Output::Single(input[..index].to_string())
-}
-
-fn trim_from_index(index: usize, input: &String) -> Output {
-    let index = if index != 0 { index } else { input.len() };
-    Output::Single(input[..index].to_string())
-}
-
-fn trim_from_index_to_index(start: usize, end: usize, input: &String) -> Output {
-    Output::Single(if end <= start {
-        input.clone()
-    } else {
-        input[..start].to_string() + &input[end..]
-    })
-}
-
-fn trim_from_index_to_offset(start: usize, offset: i64, input: &String) -> Output {
-    Output::Single(
-        input[if offset.is_negative() {
-            start + ((offset - 1) as usize)..start
-        } else {
-            start..start + offset as usize
-        }]
-        .to_string(),
-    )
-}
-
-fn trim_until_index(index: usize, input: &String) -> Output {
-    Output::Single(input[index..].to_string())
-}
-
-fn cut_from_pat_to_index(pattern: &String, index: usize, input: &String) -> Output {
-    // implement only matching after the index?
-    let found_idx = input.find(pattern).unwrap_or(0);
-    if index < found_idx {
-        eprintln!("Pattern was found before desired index");
-        std::process::exit(1);
-    } else if index == found_idx {
-        return Output::Single(input.clone());
+            input[..start].to_string() + &input[end..]
+        })
     }
 
-    Output::Single(input[found_idx..index].to_string())
-}
-
-fn cut_from_index_to_pat(index: usize, pattern: &String, input: &String) -> Output {
-    let found_idx = input.find(pattern).unwrap_or(0);
-    if index > found_idx {
-        eprintln!("First pattern instance was found after desired index");
-        std::process::exit(1);
-    } else if index == found_idx {
-        return Output::Single(input.clone());
+    pub fn trim_from_index_to_offset(start: usize, offset: i64, input: &String) -> Output {
+        Output::Single(
+            input[if offset.is_negative() {
+                start + ((offset - 1) as usize)..start
+            } else {
+                start..start + offset as usize
+            }]
+            .to_string(),
+        )
     }
 
-    Output::Single(input[index..found_idx].to_string())
-}
-
-fn trim_from_pat_to_index(pattern: &String, index: usize, input: &String) -> Output {
-    let found_idx = input.find(pattern).unwrap_or(0);
-
-    if index < found_idx {
-        eprintln!("Pattern was found before desired index");
-        std::process::exit(1);
-    } else if index == found_idx {
-        return Output::Single(input.clone());
+    pub fn trim_until_index(index: usize, input: &String) -> Output {
+        Output::Single(input[index..].to_string())
     }
 
-    Output::Single(input[..found_idx].to_string() + &input[index..])
-}
+    pub fn cut_from_pat_to_index(pattern: &String, index: usize, input: &String) -> Output {
+        // implement only matching after the index?
+        let found_idx = input.find(pattern).unwrap_or(0);
+        if index < found_idx {
+            eprintln!("Pattern was found before desired index");
+            std::process::exit(1);
+        } else if index == found_idx {
+            return Output::Single(input.clone());
+        }
 
-fn trim_from_index_to_pat(index: usize, pattern: &String, input: &String) -> Output {
-    let found_idx = input.find(pattern).unwrap_or(0);
-
-    if index > found_idx {
-        eprintln!("First pattern was found after desired index");
-        std::process::exit(1);
-    } else if index == found_idx {
-        return Output::Single(input.clone());
+        Output::Single(input[found_idx..index].to_string())
     }
 
-    Output::Single(input[..index].to_string() + &input[found_idx..])
+    pub fn cut_from_index_to_pat(index: usize, pattern: &String, input: &String) -> Output {
+        let found_idx = input.find(pattern).unwrap_or(0);
+        if index > found_idx {
+            eprintln!("First pattern instance was found after desired index");
+            std::process::exit(1);
+        } else if index == found_idx {
+            return Output::Single(input.clone());
+        }
+
+        Output::Single(input[index..found_idx].to_string())
+    }
+
+    pub fn trim_from_pat_to_index(pattern: &String, index: usize, input: &String) -> Output {
+        let found_idx = input.find(pattern).unwrap_or(0);
+
+        if index < found_idx {
+            eprintln!("Pattern was found before desired index");
+            std::process::exit(1);
+        } else if index == found_idx {
+            return Output::Single(input.clone());
+        }
+
+        Output::Single(input[..found_idx].to_string() + &input[index..])
+    }
+
+    pub fn trim_from_index_to_pat(index: usize, pattern: &String, input: &String) -> Output {
+        let found_idx = input.find(pattern).unwrap_or(0);
+
+        if index > found_idx {
+            eprintln!("First pattern was found after desired index");
+            std::process::exit(1);
+        } else if index == found_idx {
+            return Output::Single(input.clone());
+        }
+
+        Output::Single(input[..index].to_string() + &input[found_idx..])
+    }
 }
 
 // Powerful command-line-driven string manipulation tool
