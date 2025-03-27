bfc3b730fa4673cd341bf8fee9e682fe12d89357
thoth@damocles <jonathankurtz@protonmail.com>
2025-03-27 00:07:36 -0500

    added even more clap help annotations for ease-of-use

--- src/main.rs
+++ src/main.rs
@@ -8,145 +8,195 @@ enum Output {
 #[derive(Subcommand)]
 enum Operation {
     /* Pattern-Based */
+    #[command(about = "Splits at whitespace", long_about = "Splits each line at whitespace, optionally a finite number of times per line.")]
     SplitAtWhitespace {
         #[arg(help = "Optional: number of whitespace segments to split at (negative values start from end)")]
         number: Option<i64>,
     },
+
+    #[command(about = "Splits at a pattern", long_about = "Splits each line at a given pattern, optionally a finite number of times per line.")]
     SplitAtPat {
         #[arg(help = "Pattern at which to split the input")]
         pattern: String,
         #[arg(help = "Optional: number of times to split at given pattern (negative values start from end")]
         number: Option<i64>,
     },
+
+    #[command(about = "Splits at a character", long_about = "Splits each line at a given character, optionally a finite number of times per line.")]
     SplitAtChar {
         #[arg(help = "Character at which to split at.")]
         char: char,
-        #[arg(help = "Optional: number of times to split at given character (negative values start from end)")]
+        #[arg(help = "Optional: number of times to split at a given character (negative values start from end)")]
         number: Option<i64>,
     },
+
+    #[command(about = "Cuts starting at a pattern", long_about = "Cuts each line starting from a given pattern to the end of the line.")]
     CutFromPat {
         #[arg(help = "Pattern at which to begin cutting (pattern-inclusive, cuts until end of line)")]
         pattern: String,
     },
+
+    #[command(about = "Cuts between two patterns", long_about = "Cuts contents of a line between one pattern and another.")]
     CutFromPatToPat {
         #[arg(help = "Pattern at which to begin cutting (pattern-inclusive, cuts until end-pattern, or end of line)")]
         start: String,
         #[arg(help = "Pattern at which to stop cutting (pattern-exclusive, from beginning if start not found)")]
         end: String,
     },
+
+    #[command(about = "Cuts from a pattern to an offset from the pattern", long_about = "Cuts starting from a pattern to a given offset amount from the first character of the pattern.")]
     CutFromPatToOffset {
         #[arg(help = "Pattern to begin cutting at (pattern-inclusive)")]
         pattern: String,
         #[arg(help = "Offset from pattern to cut until (negative values move backward from beginning of first pattern)")]
         offset: i64,
     },
+
+    #[command(about = "Cuts until a pattern", long_about = "Cuts starting from the beginning of a line to either the first character of a pattern, or the end of the line if the pattern isn't found.")]
     CutUntilPat {
         #[arg(help = "Pattern to cut until (pattern-exclusive)")]
         pattern: String,
     },
+
+    #[command(about = "Trims a line starting from a pattern", long_about = "Trims (removes) contents of a line starting from a given pattern to the end of the line.")]
     TrimFromPat {
         #[arg(help = "Pattern to begin trimming at (pattern-inclusive)")]
         pattern: String,
     },
+
+    #[command(about = "Trims between two patterns", long_about = "Trims (removes) contents of a line between one pattern and another, or the end of the line if the second pattern isn't found.")]
     TrimFromPatToPat {
         #[arg(help = "Pattern to begin trimming from (pattern-inclusive, trims to end if end-pattern is not found)")]
         start: String,
         #[arg(help = "Pattern to stop trimming at (pattern-exclusive)")]
         end: String,
     },
+
+    #[command(about = "Trims until a pattern", long_about = "Trims (removes) contents of a line starting from the beginning of the line, until the pattern is found. If the pattern isn't found, nothing is done.")]
     TrimUntilPat {
         #[arg(help = "Pattern to trim until (pattern-exclusive)")]
         pattern: String,
     },
+
+    #[command(about = "Trims to a pattern (inclusive)", long_about = "Trims (removes) contents of a line up to and including a given pattern. If the pattern isn't found, nothing is done.")]
     TrimToPat {
         #[arg(help = "Pattern to trim to (pattern-inclusive)")]
         pattern: String,
     },
 
     /* Index-Based */
+    #[command(about = "Splits at a given index", long_about = "Split each line at a given index.")]
     SplitAtIndex {
         #[arg(help = "Index to split at")]
         index: usize,
     },
+
+    #[command(about = "Cut beginning at a given index", long_about = "Cut each line beginning at an index until the end of the line.")]
     CutFromIndex {
         #[arg(help = "Index to begin cutting from (index-inclusive, cuts until end of line)")]
         index: usize,
     },
+    
+    #[command(about = "Cut between two indices", long_about = "Cut each line between one index (inclusive) and another (exclusive).")]
     CutFromIndexToIndex {
         #[arg(help = "Index to begin cutting from (index-inclusive)")]
         start: usize,
         #[arg(help = "Index to stop cutting at (index-exclusive)")]
         end: usize,
     },
+    
+    #[command(about = "Cuts from an index to an offset from the index", long_about = "Cuts contents of a line starting from an index to a given offset amount from the index.")]
     CutFromIndexToOffset {
         #[arg(help = "Index at which to begin cutting (index-inclusive)")]
         index: usize,
         #[arg(help = "Offset from index to cut to (negative values move backward from index)")]
         offset: i64,
     },
+
+    #[command(about = "Cuts until a given index", long_about = "Cuts contents of a line starting from the beginning of each line to a given index in that line.")]
     CutUntilIndex {
         #[arg(help = "Index to cut until (index-exclusive, if zero)")]
         index: usize,
     },
+
+    #[command(about = "Trims starting from an index", long_about = "Trims (removes) contents of a line starting from a given index (inclusive) the the end of the line.")]
     TrimFromIndex {
         #[arg(help = "Index to begin trimming from (zero-based, index-inclusive)")]
         index: usize,
     },
+
+    #[command(about = "Trims between two indices", long_about = "Trims (removes) contents of a line between two indices.")]
     TrimFromIndexToIndex {
         #[arg(help = "Index to begin trimming from (zero-based, index-inclusive)")]
         start: usize,
         #[arg(help = "Index to stop trimming at (zero-based, index-exclusive)")]
         end: usize,
     },
+
+    #[command(about = "Trims from an index ton an offset from the index", long_about = "Trims (removes) starting from an index to a given offset amount from the index.")]
     TrimFromIndexToOffset {
         #[arg(help = "Index to start trimming from (zero-based, index-inclusive)")]
         index: usize,
         #[arg(help = "Offset from index to trim to (negative values move backward from index)")]
         offset: i64,
     },
+
+    #[command(about = "Trims until an index", long_about = "Trims (removes) contents of a line until a given index.")]
     TrimUntilIndex {
         #[arg(help = "Index to trim until (zero-baesd, index-exclusive)")]
         index: usize,
     },
+    
+    #[command(about = "Trims whitespace or patterns from both ends", long_about = "Trims (removes) either whitespace (default) or patterns from the beginning and end of each line.")]
     Trim {
         #[arg(help = "Optional: pattern to trim from beginning and end of input")]
         pattern: Option<String>,
     },
+    
+    #[command(about = "Replaces one pattern with another", long_about = "Replaces instances of a given pattern with another, optionally a finite number of times per line (to remove patterns entirely, use remove command).")]
     Replace {
         #[arg(help = "Pattern to replace inline from input")]
         pattern: String,
-        #[arg(help = "What to replace pattern with (to remove patterns, use remove command)")]
+        #[arg(help = "What to replace pattern with")]
         with: String,
-        #[arg(help = "Optional: number of whitespace segments to split at (negative values start from end)")]
+        #[arg(help = "Optional: number of pattern-matches to replace (negative values start from end)")]
         number: Option<i64>,
     },
-
+    
+    #[command(about = "Removes a pattern", long_about = "Removes inline a given pattern, optioanlly a finite number of times per line.")]
     Remove {
         #[arg(help = "Pattern to remove inline from input")]
         pattern: String,
-        #[arg(help = "Optional: number of whitespace segments to split at (negative values start from end)")]
+        #[arg(help = "Optional: number of pattern-matches to remove (negative values start from end)")]
         number: Option<i64>,
     },
 
     /* Mixed */
+    #[command(about = "Cuts from a pattern to an index", long_about = "Cuts contents of a line starting at the first character of a pattern to a given index (exclusive).")]
     CutFromPatToIndex {
         #[arg(help = "Pattern to begin cutting at (pattern-inclusive)")]
         pattern: String,
         #[arg(help = "Index to stop cutting at, must be after pattern (zero-based, index-exclusive)")]
         index: usize,
     },
+
+    #[command(about = "Cuts from an index to a pattern", long_about = "Cuts contents of a line starting from a given index (inclusive) and the first character of a pattern.")]
     CutFromIndexToPat {
         #[arg(help = "Index to begin cutting at (zero-based, index-inclusive)")]
         index: usize,
         #[arg(help = "Pattern to stop cutting at (pattern-exclusive)")]
         pattern: String,
     },
+    
+    #[command(about = "Trims between a pattern and an index", long_about = "Trims (removes) contents of a line between the firts character of a pattern and a given index in the line.")]
     TrimFromPatToIndex {
         #[arg(help = "Patter to begin trimming from (pattern-inclusive)")]
         pattern: String,
         #[arg(help = "Index to stop trimming at (index-exclusive)")]
         index: usize,
     },
+    
+    #[command(about = "Trims between an index and a pattern", long_about = "Trims (removes) contents of a line starting from a given index (inclusive) and the first character of a pattern.")]
     TrimFromIndexToPat {
         #[arg(help = "Index to begin trimming from (index-inclusive)")]
         index: usize,
